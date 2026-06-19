ff44814c4b3e73d1d04fd9b04cd50e21ea3792ff
Amarnathdj10 <amarnathdj56@gmail.com>
2026-06-19 20:32:32 +0530

    uefa 2026

--- win_percent_2026.py
+++ win_percent_2026.py
@@ -3,5 +3,52 @@ import pandas as pd
 df = pd.read_csv('prediction_dataset.csv')
 
 stats = [
-    [],
+    [34,21,75,29,70], #austria
+    [34,18,74,29,63], #belgium
+    [29,9,16,53,31], #bosnia
+    [33,22,78,43,66], #croatia
+    [33,18,52,34,63], #czechia
+    [37,27,76,24,86], #england
+    [36,24,90,37,78], #france
+    [36,19,74,43,64]  #germany
+    [36,20,93,44,67], #netherlands
+    [30,19,82,37,62], #norway
+    [36,27,107,39,84], #portugal
+    [33,14,51,50,48], #scotland
+    [37,30,114,45,94], #spain
+    [33,17,65,47,55], #sweden
+    [35,15,61,41,59], #sswitz
+    [35,19,63,51,63], #turkeey
+    [], #argentina
+    [], #brazil
+    [], #colombia
+    [], #ecuador
+    [], #paraguay
+    [], #uruguay
+    [], #algeria
+    [], #cabo
+    [], #congo
+    [], #ivory
+    [], #egypt
+    [], #ghana
+    [], #morocco
+    [], #senegal
+    [], #sa
+    [], #tunisia
+    [], #canada
+    [], #mexico
+    [], #usa
+    [], #curacao
+    [], #haiti
+    [], #panama
+    [], #australia
+    [], #iran
+    [], #iraq
+    [], #japan
+    [], #jordan
+    [], #korea
+    [], #qatar
+    [], #saudi
+    [],#uzbekistan
+    [], #new zealand
 ]
